b6504f5c264ca80eb9147db82f12717f60f5df95
FONTENAY Clément <SJKG4794@ad-subs.w2k.francetelecom.fr>
2020-01-03 15:13:26 +0100

    installation

--- web/WebApplication/WebApplication/Startup.cs
+++ web/WebApplication/WebApplication/Startup.cs
@@ -5,9 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using WebApplication.Models;
 
 namespace WebApplication
 {
@@ -23,6 +25,9 @@ namespace WebApplication
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+            services.AddEntityFrameworkSqlServer()
+                    .AddDbContext<DbContext>(options => options.UseSqlServer(Configuration["Data:ConnectionString"]));
             services.AddControllersWithViews();
         }
 
